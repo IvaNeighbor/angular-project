f7bf0ec960cdc00705b50bc2528dea00231662ef
Ivan <duzak2006@gmail.com>
2025-10-28 10:15:43 +0200

    start lab_2

---
+++ src/app/destination-section/destination-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destination-section',
+  imports: [],
+  templateUrl: './destination-section.html',
+  styleUrl: './destination-section.scss'
+})
+export class DestinationSection {
+
+}
---
+++ src/app/destination-section/trip-cards/trip-cards.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-cards',
+  imports: [],
+  templateUrl: './trip-cards.html',
+  styleUrl: './trip-cards.scss'
+})
+export class TripCards {
+
+}
